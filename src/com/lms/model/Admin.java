9cbdef70ea7638d2f3068a9cf808bb42816309e5
Masum Reza <masum.reza@SIPL.local>
2017-07-31 20:08:18 +0530

    commit message

---
+++ src/com/lms/model/Admin.java
@@ -0,0 +1,19 @@
+package com.lms.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+/*@Entity
+@Table(name="Admin")*/
+public class Admin implements Serializable{
+
+	/**
+	 * 
+	 */
+	/*private static final long serialVersionUID = 1L;
+	@Column(name="")
+	private String name;*/
+
+}
